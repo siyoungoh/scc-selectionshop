dbdf2494a08ead725b38b5c95a97fc7f3e5fda69
siyoungoh <dev@syoh.net>
2021-11-10 12:25:57 +0900

    06-12 user db access issue solved

--- src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
@@ -16,6 +16,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 			// image 폴더를 login 없이 허용
 			.antMatchers("/user/**").permitAll()
+			// h2-console URL 을 login 없이 허용
+			.antMatchers("/h2-console/**").permitAll()
 			.antMatchers("/images/**").permitAll()
 			// css 폴더를 login 없이 허용
 			.antMatchers("/css/**").permitAll()
